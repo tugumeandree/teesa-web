e2e7e3c422b7af0f89e631eb4272746b52fbb43f
Tugume Andrew <andrewtugume2@gmail.com>
2020-10-07 13:15:05 +0300

    update

--- src/login/signInScreen.js
+++ src/login/signInScreen.js
@@ -1,7 +1,7 @@
 // Import FirebaseAuth and firebase.
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 
 // Initialize Firebase
  const config = {
